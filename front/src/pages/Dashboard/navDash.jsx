e02a9effc252cfed26ccbea044d58b28ffd72fff
TANHEHANZ <hantach10@gmail.com0>
2023-09-07 14:46:38 -0400

    styleNavCorecciion

--- front/src/pages/Dashboard/navDash.jsx
+++ front/src/pages/Dashboard/navDash.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-import { Container ,NavLink} from "../../styles/styleGlobal";
+import { Container, NavLink } from "../../styles/styleGlobal";
 const NavDash = () => {
+  const [expandir, setExpandir] = useState(false);
   return (
     <Container>
       <nav>
         <NavLink to="/dashboard/Panel">Panel General</NavLink>
         <NavLink to="/dashboard/Mapa">Mapa</NavLink>
-        <NavLink to="/dashboard/Sensor1">Sensor 1</NavLink>
-        <NavLink to="/dashboard/Sensor2">Sensor 2</NavLink>
-        <NavLink to="/dashboard/Sensor3">Sensor 3</NavLink>
-        <NavLink to="/dashboard/Sensor4">Sensor 4</NavLink>
+        <button onClick={() => setExpandir(!expandir)}>Sensores</button>
+        {expandir && (
+          <article>
+            <NavLink to="/dashboard/Sensor1">Sensor 1</NavLink>
+            <NavLink to="/dashboard/Sensor2">Sensor 2</NavLink>
+            <NavLink to="/dashboard/Sensor3">Sensor 3</NavLink>
+            <NavLink to="/dashboard/Sensor4">Sensor 4</NavLink>
+          </article>
+        )}
         <NavLink to="/dashboard/Graficos">Graficos Comparativos</NavLink>
         <NavLink to="/dashboard/Filtro">Filtro de datos</NavLink>
         <NavLink to="/dashboard/Historicos">Historicos Generales</NavLink>
--- front/src/styles/styleGlobal.jsx
+++ front/src/styles/styleGlobal.jsx
@@ -13,38 +13,72 @@ export const Container = styled.section`
     flex-direction:column;
     gap: 2em;
     background-color: ${colors.primary};
+    & article {
+      display: flex;
+      flex-direction: column;
+      width:80%;
+    }
+    & button {
+      text-align: start;
+      background-color: transparent;
+      border: none;
+      color: ${colors.light};
+      width: 100%;
+      padding: 0.5em 2.1em;
+      transition: all 0.3s ease-in-out;
+      border-bottom: solid 1px ${colors.light};
+      &::after {
+        content: "";
+        position: absolute;
+        width: 10px;
+        height: 10px;
+        background-color: ${colors.light};
+        left: 0.5em;
+        top: 40%;
+        border-radius: 50% 0;
+      }
+      &:hover {
+        background-color: ${colors.secundary};
+        color: ${colors.primary};
+        transform: translateX(0.5em);
+        border-radius: 0 0.3em 0.3em 0;
+        &::after {
+          background-color: ${colors.primary};
+        }
+      }
+    }
   }
   & > section {
     width: calc(15vw - 100vw);
-    height:100vh;
+    height: 100vh;
   }
 `;
 
 export const NavLink = styled(Link)`
   text-decoration: none;
   color: ${colors.light};
-  width:100%;
-  padding:0.5em  2em;
-  border-bottom:solid 1px ${colors.light};
-  position:relative;
-  transition:all .3s ease-in-out;
-  &::after{
-    content:"";
-    position:absolute;
-    width:10px;
-    height:10px;
-    background-color:${colors.light};
-    left:0.5em;
-    top:40%;
-    border-radius:50% 0;
+  width: 100%;
+  padding: 0.5em 2em;
+  border-bottom: solid 1px ${colors.light};
+  position: relative;
+  transition: all 0.3s ease-in-out;
+  &::after {
+    content: "";
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    background-color: ${colors.light};
+    left: 0.5em;
+    top: 40%;
+    border-radius: 50% 0;
   }
-  &:hover{
-    background-color:${colors.secundary};
-    color:${colors.primary};
-    transform:translateX(.5em);
-    border-radius:0 .3em .3em 0;
-    &::after{
-        background-color:${colors.primary};
+  &:hover {
+    background-color: ${colors.secundary};
+    color: ${colors.primary};
+    transform: translateX(0.5em);
+    border-radius: 0 0.3em 0.3em 0;
+    &::after {
+      background-color: ${colors.primary};
     }
   }
 `;
